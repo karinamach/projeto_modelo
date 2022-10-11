d09bfd59f66121b0162af4adc576a7f5c5de7001
Karina <113393760+karinamach@users.noreply.github.com>
2022-10-10 21:24:15 -0300

    Aula 10/10/2022

---
+++ Aula 10.10/aula4.js
@@ -0,0 +1,32 @@
+//Criando a Classe Calculadora
+class Calculadora{
+    //Instanciar as Características
+    constructor(num1,num2){
+        this.num1=num1
+        this.num2=num2
+    }
+    //Criando o Método Soma
+    soma(){
+        return parseInt(this.num1)+parseInt(this.num2)
+    }
+    subtracao(){
+        return parseInt(this.num1)-parseInt(this.num2)
+    }
+    multiplicacao(){  
+        return parseInt(this.num1)*parseInt(this.num2)
+    }
+    divisao(){
+        return parseInt(this.num1)/parseInt(this.num2)
+    }
+    //Criando a Requisição GET
+    get resuldado(){
+        alert("O valor da Soma é: "+this.soma()+"\nO valor da Subtracao é: "+this.subtracao()+
+        "\nO valor multiplicaçao é: "+this.multiplicacao()+"\nO valor da Divisão é: "+this.divisao())
+      }
+}
+//Coletando os Dados
+botao=document.querySelector("#btn")
+botao.onclick = function(){
+    valores=new Calculadora(document.form1.n1.value,document.form1.n2.value)
+    valores.resuldado
+}
